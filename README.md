dd3d7c132cb6c7bfb73d8e29bd64a0a371e5144e
yuyubujue <1936967743@qq.com>
2023-09-10 01:45:52 +0800

    check_gsl updated

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ version: 1.0.0
 
 如果要commit名单，需要在commit里附上gsl发言或网暴的证据。
 
+version: 1.0.1
+
+新增功能，判断该uid是否是gsl
--- check_gsl.py
+++ check_gsl.py
@@ -11,3 +11,14 @@ try:
 
 except:
     print("请输入uid，目前不支持用户名检索")
+
+insert_gsl = input("是否录入其为gsl(y/n)")
+if insert_gsl == "y" or insert_gsl == "Y":
+    with open('uid_list.txt','a') as f:
+        f.write("\n%s" % gsl_id)
+    print("已录入")
+    f.close()
+elif insert_gsl == "n" or insert_gsl == "N":
+    print("不录入")
+else:
+    print("请输入y/n")
