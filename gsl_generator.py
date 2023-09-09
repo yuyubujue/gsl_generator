b39ac78f254d520030918083b075cd56cac9cff5
yuyubujue <1936967743@qq.com>
2023-09-10 01:47:17 +0800

    Update gsl_generator.py

--- gsl_generator.py
+++ gsl_generator.py
@@ -2,7 +2,6 @@ import random
 
 with open('uid_list.txt','r') as f:
     lines = f.readlines()
-print(lines)
 rad_int = random.randint(0,len(lines))
 gsl_id = lines[rad_int]
 gsl_link = 'https://space.bilibili.com/' + gsl_id.strip()
