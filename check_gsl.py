cf1385a05ea23bf92cb5ad416df32b668848ba42
yuyubujue <1936967743@qq.com>
2023-09-10 01:58:33 +0800

    Update check_gsl.py

--- check_gsl.py
+++ check_gsl.py
@@ -3,22 +3,29 @@ with open('uid_list.txt','r') as f:
 
 gsl_id = input("uid:")
 
+cnt = 0
 try:
-    if int(gsl_id) in lines:
+    for i in lines:
+        if int(gsl_id) == int(i):
+            cnt+=1
+        else:
+            pass
+    if cnt!=0:
         print('纯正gsl')
     else:
         print('该id目前不在gsl库中')
+        insert_gsl = input("是否录入其为gsl(y/n)")
+        if insert_gsl == "y" or insert_gsl == "Y":
+            with open('uid_list.txt', 'a') as f:
+                f.write("\n%s" % gsl_id)
+            print("已录入")
+            f.close()
+        elif insert_gsl == "n" or insert_gsl == "N":
+            print("不录入")
+        else:
+            print("请输入y/n")
 
 except:
     print("请输入uid，目前不支持用户名检索")
 
-insert_gsl = input("是否录入其为gsl(y/n)")
-if insert_gsl == "y" or insert_gsl == "Y":
-    with open('uid_list.txt','a') as f:
-        f.write("\n%s" % gsl_id)
-    print("已录入")
-    f.close()
-elif insert_gsl == "n" or insert_gsl == "N":
-    print("不录入")
-else:
-    print("请输入y/n")
+
